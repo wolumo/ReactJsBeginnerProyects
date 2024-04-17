74a6f7f0213cca1a19282fd5c6bdb1777dec9778
wolumo <wistonjosequintanaosorio@hotmail.com>
2024-04-17 16:10:08 -0600

    feat: Adding Styles and All Functionality

--- calculadora-interactiva/src/App.js
+++ calculadora-interactiva/src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import BotonClear from './componentes/BotonClear';
+import Pantalla from './componentes/Pantalla';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
+import { Component, useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    setInput(input+ val);
+  };
+
+  const calcularResultado = () => { 
+
+    if(input){
+      setInput(evaluate(input));
+    } else {
+      alert("Por favor ingrese valores para realizar los calculos");
+
+    }
+
+    
+  }
+
+  const limpiarResultado = () => {
+    setInput("")
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          src={freeCodeCampLogo}
+          className="freecodecamp-logo" 
+          alt="Logo de FreeCodeCamp" />
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={input}/> 
+        <div className="fila">
+          <Boton manejarClic = {agregarInput}>1</Boton>
+          <Boton manejarClic = {agregarInput}>2</Boton>
+          <Boton manejarClic = {agregarInput}>3</Boton>
+          <Boton manejarClic = {agregarInput}>+</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic = {agregarInput}>4</Boton>
+          <Boton manejarClic = {agregarInput}>5</Boton>
+          <Boton manejarClic = {agregarInput}>6</Boton>
+          <Boton manejarClic = {agregarInput}>-</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic = {agregarInput}>7</Boton>
+          <Boton manejarClic = {agregarInput}>8</Boton>
+          <Boton manejarClic = {agregarInput}>9</Boton>
+          <Boton manejarClic = {agregarInput}>*</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClic = {calcularResultado}>=</Boton>
+          <Boton manejarClic = {agregarInput}>0</Boton>
+          <Boton manejarClic = {agregarInput}>.</Boton>
+          <Boton manejarClic = {agregarInput}>/</Boton>
+        </div>
+        <div className="fila">
+          <BotonClear manejarClic = {limpiarResultado}>Clear</BotonClear>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ calculadora-interactiva/src/componentes/Pantalla.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../hojas-de-estilo/Pantalla.css"
+
+const Pantalla = ({ input }) => ( /*This Structure could be used when te component to do is very simple*/
+  <div className="input">
+    {input}
+  </div>
+); 
+
+
+export default Pantalla; 
